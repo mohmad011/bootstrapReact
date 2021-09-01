c960b0ef55c7570fcc5dddc8fa24612d5ebe7745
mohmad011 <68702995+mohmad011@users.noreply.github.com>
2021-09-01 15:57:44 +0200

    add

---
+++ cds.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# md Contact
+# chmod +x Contact 
+cd Contact
+touch 'index.js'
+cd ../
+
+# md Explore
+# chmod +x Explore 
+cd Explore
+touch 'index.js'
+cd ../
+
+# md Profile
+# chmod +x Profile 
+cd Profile
+touch 'index.js'
+cd ../
+
+# md Authors
+# chmod +x Authors 
+cd Authors
+touch 'index.js'
+cd ../
+
+# md CreateItem
+# chmod +x  CreateItem
+cd CreateItem
+touch 'index.js'
+cd ../
+
+# md Home
+# chmod +xmeser 
+cd Home
+touch 'index.js'
+cd ../
+
+# md SignIn
+# chmod +xSignInr 
+cd SignIn
+touch 'index.js'
+cd ../
+
+# md ConnectWallet
+# chmod +x  ConnectWallet
+cd ConnectWallet
+touch 'index.js'
+cd ../
+
+# md Explore2
+# chmod +x  Explore2
+cd Explore2
+touch 'index.js'
+cd ../
+
+# md ItemDetails
+# chmod +x  ItemDetails
+cd ItemDetails
+touch 'index.js'
+cd ../
+
+# md SignUp
+# chmod +xSignUpr 
+cd SignUp
+touch 'index.js'
+cd ../
